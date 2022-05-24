0cedfa8f404c5c96eb1fd0b6617417fe7311c4ee
nahowo <96415630+nahowo@users.noreply.github.com>
2022-05-24 23:41:03 +0900

    Add files via upload

---
+++ ch3.cpp
@@ -0,0 +1,15 @@
+//chapter 3
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    double x1, y1;
+    cout << "Enter first number: ";
+    cin >> x1;
+    cout << "Enter first number: ";
+    cin >> y1;
+    cout << x1 << "/" << y1 << "=" << x1/y1 << "\n";
+    return 0;
+}
